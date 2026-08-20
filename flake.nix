8bc64a97f3b97aab7b9bfa5cdc82981a2887d988
Paul Smith <paulsmith@pobox.com>
2026-08-19 21:00:04 -0500

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "litefind - grep for SQLite dbs";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      pkgsFor = system: import nixpkgs { inherit system; };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "litefind";
+            version = "0.1.0";
+            src = ./.;
+            vendorHash = "sha256-BAvfNq8jRMtxnNRnCfD4m3N9Yqc7o9dM/v6eVfK0Iag=";
+            ldflags = [
+              "-s"
+              "-w"
+            ];
+            meta = {
+              description = "litefind - grep for SQLite dbs";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "litefind";
+            };
+          };
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${nixpkgs.lib.getExe self.packages.${system}.default}";
+          meta = self.packages.${system}.default.meta;
+        };
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ pkgs.go_1_26 pkgs.golangci-lint ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: (pkgsFor system).nixfmt);
+    };
+}
